83702a935aaf9ee2cee0e612ef3f5bbb5efd6a5f
pedro558 <pedroafonso109@gmail.com>
2025-07-07 08:02:19 -0300

    creating first user test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
@@ -12,6 +12,15 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
+# view login
+login_manager.login_view = 'login'
+# Session <- conexão ativa
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
 @app.route('/login', methods=['POST'])
 def login():
   data = request.json
@@ -19,8 +28,12 @@ def login():
   password = data.get('password')
 
   if username and password:
-    # Login
-    pass
+    #login
+    user = User.query.filter_by(username=username).first()
+
+    if user and user.password == password:
+        login_user(user)
+        return jsonify({'message': 'Autenticação realizada com sucesso'}), 200
 
   return jsonify({'message': 'Credenciais inválidas'}), 400
 

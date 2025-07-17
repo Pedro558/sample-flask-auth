749db70a3205b1f3d8955e960a3d62cea39e7bf2
pedro558 <pedroafonso109@gmail.com>
2025-07-17 09:39:38 -0300

    feat: add user logout and creation endpoints; improve authentication response

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
@@ -33,9 +33,29 @@ def login():
 
     if user and user.password == password:
         login_user(user)
-        return jsonify({'message': 'Autenticação realizada com sucesso'}), 200
+        print(current_user.is_authenticated)
+        return jsonify({'message': 'Autenticacao realizada com sucesso'}), 200
 
   return jsonify({'message': 'Credenciais inválidas'}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({'message': 'Logout realizado com sucesso'}), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+     user = User(username=username, password=password)
+     db.session.add(User)
+     db.session.commit()
+     return jsonify({'message': 'Usuário criado com sucesso'}), 201
+  return jsonify({'message': 'Dados inválidos'}), 400
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
